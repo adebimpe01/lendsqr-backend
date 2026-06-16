312f39ef0b4140d517b5530b84047370154ba841
Okiki <abdulkareemrukayat18@gmail.com>
2026-06-16 04:46:45 +0100

    fix json-server deployment

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
